9f25ae4f819bffc7f798d44a6a08fca2742ff82f
javiergalindo-sudo <javiergalindo@casadelsoftware.com>
2026-06-08 22:49:22 -0400

    server

---
+++ server.py
@@ -0,0 +1,46 @@
+''' Executing this function initiates the application of emotion detection to be executed over the Flask channel and deployed on
+    localhost:5000.
+'''
+# Import Flask, render_template, request from the flask pramework package : TODO
+# Import the emotion_detector function from the package created: TODO
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+#Initiate the flask app : TODO
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def sent_detection():
+    """Analyze the sentiment of the provided text and return the result."""
+    # Retrieve the text to analyze from the request arguments
+    text_to_analyze = request.args.get('textToAnalyze')
+
+    # Pass the text to the emotion_detector function and store the response
+    response = emotion_detector(text_to_analyze)
+
+    # Extraer los valores del diccionario
+    anger = response['anger']
+    disgust = response['disgust']
+    fear = response['fear']
+    joy = response['joy']
+    sadness = response['sadness']
+    dominant_emotion = response['dominant_emotion']
+    
+    # Formatear la respuesta en el formato requerido
+    formatted_response = (
+        f"For the given statement, the system response is "
+        f"'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, "
+        f"'joy': {joy} and 'sadness': {sadness}. "
+        f"The dominant emotion is {dominant_emotion}."
+    )
+    
+    # Retornar la respuesta formateada
+    return formatted_response
+
+@app.route("/")
+def render_index_page():
+    """Render the main index page."""
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
